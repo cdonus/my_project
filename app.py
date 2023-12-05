cdf20bcacbf089589c100e9462d8ffb7c52a4cb6
cdonus <151811990+cdonus@users.noreply.github.com>
2023-12-05 20:59:32 +0400

    archivos modificado app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import streamlit as st
 st.header('Vehiculos')
         
 car_data = pd.read_csv('/Users/user/my_project/vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir Histograma') # crear un botón
-
+build_histogram = st.checkbox('Construir un histograma')
+build_grafico = st.checkbox('Construir Grafico Dispersion') # crear un botón
         
-if hist_button: # al hacer clic en el botón
+if build_histogram: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
@@ -18,8 +18,8 @@ if hist_button: # al hacer clic en el botón
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-hist_button_2 = st.button('Construir Grafico Dispersion') # crear un botón
-if hist_button_2: # al hacer clic en el botón
+
+if build_grafico: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
             
